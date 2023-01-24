97ccc5bd39df91866db72cc19894892bad4a1abe
codewithdivu <mavadiyadivyesh56@gmail.com>
2023-01-24 17:50:36 +0530

    added delete request

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const { raw } = require("express");
 const express = require("express");
 const app = express();
 require("./db/connection");
@@ -39,6 +40,44 @@ app.post("/student", async (req, res) => {
   }
 
   res.send("hello from POST");
+});
+
+app.get("/student", async (req, res) => {
+  try {
+    const studentsData = await Student.find();
+    res.send(studentsData);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.get("/student/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const studentData = await Student.findById(_id);
+    console.log("studentData", studentData);
+
+    if (!studentData) {
+      res.status(404).send();
+    } else {
+      res.send(studentData);
+    }
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.delete("/student/:id", async (req, res) => {
+  try {
+    const deleteStudent = await Student.findByIdAndDelete(req.params.id);
+    if (!req.params.id) {
+      res.status(400).send();
+    } else {
+      res.send(deleteStudent);
+    }
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
 app.listen(port, () => {
